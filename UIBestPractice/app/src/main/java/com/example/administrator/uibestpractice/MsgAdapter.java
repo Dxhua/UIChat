0176ee94645d1d529c500a6bf998407e86a51f66
Dxhua <dxhua1996@qq.com>
2017-09-20 14:29:29 +0800

    change

--- UIBestPractice/app/src/main/java/com/example/administrator/uibestpractice/MsgAdapter.java
+++ UIBestPractice/app/src/main/java/com/example/administrator/uibestpractice/MsgAdapter.java
@@ -1,7 +1,6 @@
 package com.example.administrator.uibestpractice;
 
 import android.support.v7.widget.RecyclerView;
-import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
